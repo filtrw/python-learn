eeb3081103ff6dee16fe1f911dc49b7cce377c1f
MariyaPastukhova <filtrw@gmail.com>
2020-12-19 14:53:22 +0300

    [NOISSUE] add descriptions and task number

--- level2_basics_and_application/count_type_of_crimes.py
+++ level2_basics_and_application/count_type_of_crimes.py
@@ -1,5 +1,7 @@
 """
-Вам дана частичная выборка из датасета зафиксированных преступлений, совершенных в городе Чикаго с 2001 года по настоящее время.
+Task-3.4.1
+Вам дана частичная выборка из датасета зафиксированных преступлений, совершенных в городе Чикаго с 2001 года по
+настоящее время.
 Одним из атрибутов преступления является его тип – Primary Type.
 Вам необходимо узнать тип преступления, которое было зафиксировано максимальное число раз в 2015 году.
 """
@@ -18,7 +20,7 @@ with open(crimes_file_path) as crimes_file:
     reader_crimes = csv.DictReader(crimes_file)
     for row in reader_crimes:
         if date_start <= datetime.strptime(row["Date"], "%m/%d/%Y %I:%M:%S %p") <= date_end:
-            if (row["Primary Type"] not in count_crime_types):
+            if row["Primary Type"] not in count_crime_types:
                 count_crime_types[row["Primary Type"]] = 0
             count_crime_types[row["Primary Type"]] += 1
 
