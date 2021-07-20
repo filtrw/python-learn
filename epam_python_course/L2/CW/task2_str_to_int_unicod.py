6e9d0e2fa90db80c3b9de0aca341d9a952f54618
filtrw <filtrw@gmail.com>
2021-07-20 11:31:06 +0300

    [EPAM-L2] complete CW.task2

--- epam_python_course/L2/CW/task2_str_to_int_unicod.py
+++ epam_python_course/L2/CW/task2_str_to_int_unicod.py
@@ -8,12 +8,23 @@ import math
 
 
 def main():
-    string_to_convert = input()
+    string_to_convert = input('Please, input string to convert ')
     int_from_string = 0
-    count_chr_in_str = len(string_to_convert)
-    for index in range(count_chr_in_str - 1, -1, -1):
-        print(ord(string_to_convert[index]))
-        # print(math.floor(math.log10(ord(string_to_convert[index]))))
+    ten_power = 0
+    count_chr_in_str = len(string_to_convert) - 1
+    for index in range(count_chr_in_str, -1, -1):
+        if index == count_chr_in_str:
+            int_from_string = ord(string_to_convert[index])
+        else:
+            previous = ord(string_to_convert[index + 1])
+            current = ord(string_to_convert[index])
+            if previous < 100:
+                ten_power += 2
+            elif previous >= 100:
+                ten_power += 3
+
+            int_from_string += current * 10 ** ten_power
+    print(f'Converted string in int is {int_from_string}')
     return int_from_string
 
 
