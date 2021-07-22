9fa657dfe1441cd4a374289fa414d147f32991cf
filtrw <filtrw@gmail.com>
2021-07-22 12:17:47 +0300

    [EPAM-L2] HW.task1 - add implementation menu function and complete task

--- epam_python_course/L2/HW/task1_phonebook.py
+++ epam_python_course/L2/HW/task1_phonebook.py
@@ -11,14 +11,12 @@
 человека, затем вводит номер
 6. При выборе вывода номер(а/ов), они выводятся в формате таблицы, где имя человека
 может повторятся
+       Пример:
+       имя         номер
+       Кирилл      11111111
+       Кирилл      22222222
 
-Пример:
-
-имя         номер
-Кирилл      11111111
-Кирилл      22222222
-
-При выборе удаления человека, пользователь вводит имя человека. Программа ищет этого человека и
+7. При выборе удаления человека, пользователь вводит имя человека. Программа ищет этого человека и
 удаляет его если он есть, если его нет - выводит на экран что человека нет в справочнике.
 
 Дополнительно
@@ -29,14 +27,51 @@
 # key is name of man and value is list of phone number
 # For example:
 # {'Joe': ['111111111', '222222222']}
+import sys
+
 phonebook = {}
+
+
+def menu():
+    """
+    Console menu for select action and call selected function.
+    In case of incorrect input text  will be displayed error message.
+
+    :return:
+    """
+    welcome_text = "Please, choose action with phonebook: \nInput ADD for adding phone number \nInput PRINT for " \
+                   " display phone number \nInput DEL for deleting phone number\n"
+
+    add_func = "ADD"
+    output_func = "PRINT"
+    delete_func = "DEL"
+
+    current_selection = input(welcome_text)
+
+    if current_selection == add_func:
+        input_name = input("Please, input name for adding phone number ")
+        input_number = input("Please, input phone number ")
+        add_number_by_name(input_name, input_number)
+    elif current_selection == output_func:
+        input_name = input("Please, input name for display phone numbers ")
+        output_numbers_by_name(input_name)
+    elif current_selection == delete_func:
+        input_name = input("Please, input name for deleting phone numbers ")
+        delete_by_name(input_name)
+    else:
+        print("Incorrect command for action. Please, input ADD or PRINT or DEL")
 
 
 def add_number_by_name(name: str, phonenumber: str):
     """
+    Adding record in phonebook. In case when name is absent - will be added new record,
+    in case when name already exists new number will be added in list of phone number for this name
 
-    :param name:
-    :param phonenumber:
+    :param name: Name of man for record in phonebook
+    :type name: str
+
+    :param phonenumber: Phonenumber for record in phonebook
+    :type phonenumber: str
     :return:
     """
 
@@ -48,17 +83,27 @@ def add_number_by_name(name: str, phonenumber: str):
 
 def delete_by_name(name: str):
     """
+    Deleting record in phonebook. Will be deleted all phonenumbers for input name.
+    In case if name is absent in Phonebook will be displayed a message about this.
 
-    :param name:
+    :param name: Name of man in phonebook
+    :type name: str
     :return:
     """
-    phonebook.pop(name)
+    if name in phonebook:
+        phonebook.pop(name)
+    else:
+        print(f"Man with name is {name} not found in phonebook")
 
 
 def output_numbers_by_name(name: str):
     """
+    Display name and all phonenumbers which belong for this name.
+    Format of displaing is table
 
-    :param name:
+
+    :param name:Name of man in phonebook
+    :type name: str
     :return:
     """
 
@@ -69,6 +114,15 @@ def output_numbers_by_name(name: str):
     print(f'имя \t номер')
     for phone in phonebook[name]:
         print(f'{name} \t {phone}')
+
+
+def main():
+    while True:
+        menu()
+
+
+if __name__ == '__main__':
+    main()
 
 
 def test_add_number_by_name():
@@ -105,6 +159,22 @@ def test_delete_by_name():
     delete_by_name('Eric')
     delete_by_name('Alex')
 
+    assert phonebook == reference_phonebook
+
+
+def test_delete_by_incorrect_name(capsys):
+    global phonebook
+    phonebook = {
+        'Joe': ['111111111', '333333333'],
+        'Kate': ['222222222']
+    }
+
+    reference_phonebook = phonebook.copy()
+    name = "Peter"
+    delete_by_name(name)
+    captured = capsys.readouterr()
+
+    assert captured.out == f"Man with name is {name} not found in phonebook\n"
     assert phonebook == reference_phonebook
 
 
