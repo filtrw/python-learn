38c57eac96f2df053a04086850b5543f0651f3e0
filtrw <filtrw@gmail.com>
2021-08-09 16:40:53 +0300

    [EPAM-L2] Fix review remarks

--- epam_python_course/L2/HW/task2_select_filter_dict.py
+++ epam_python_course/L2/HW/task2_select_filter_dict.py
@@ -62,51 +62,43 @@ def field_filter(field_name: str, collection: list):
     :rtype: dict
     """
 
+    collection_set = set(collection)
+
     def inner_filter(record: dict):
-        if record[field_name] in collection:
+        if record[field_name] in collection_set:
             return record
         return None
 
     return inner_filter
 
 
-def query(*args):
+def query(collection_list: list, select, *filters) -> list:
     """
     Query executed filter and selection on transferred list of records
-    The function has a number of assumptions:
-    1. Function can work with several lists of records. Result will be present in one aggregated list
-    2. Select operation always should be before filter operation in function call.
-    3. Select operation will be applied in reverse order: last input first applied. Selection fields from several
-       selection calls will not aggregate, only intersection will work correctly.
-    4. Function can work with several filters. Filters will be applied in reverse order: last input first applied
 
 
-    :param args: Lists of record for filtering and selection, select requests, filter requests
+
+    :param collection_list: list of record (dict) for selection and filtration
+    :type collection_list: list
+    :param select: function to select chosen fields from original record
+    :type select: function
+    :param *filters: several filter functions with different fields
+    :type *filters: function
 
     :return: new collection respond filters and selection
     :rtype: list of dict
     """
     filtered_collection = list()
-    input_collection = []
-    action_collection = []
-
-    # Define input args: list of record or function (selection, filter).
-    for arg in args:
-        if type(arg) is list:
-            input_collection.extend(arg)
-        else:
-            # insert in beginning needs to place filters before select
-            action_collection.insert(0, arg)
 
     # Take each record and verify filter condition and then select required fields
-    for record in input_collection:
+    for record in collection_list:
         updated_record = record
-        for func in action_collection:
-            updated_record = func(record)
+        for filter_action in filters:
+            updated_record = filter_action(record)
             if updated_record is None:
                 break
         if updated_record is not None:
-            filtered_collection.append(updated_record)
+            filtered_collection.append(select(updated_record))
 
     return filtered_collection
 
@@ -131,3 +123,26 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+def test_query():
+    friends = [
+        {'name': 'Сэм', 'gender': 'Мужской', 'sport': 'Баскетбол', 'email': 'email@email.com'},
+        {'name': 'Эмили', 'gender': 'Женский', 'sport': 'Волейбол', 'email': 'email1@email1.com'},
+        {'name': 'Питер', 'gender': 'Мужской', 'sport': 'Футбол', 'email': 'email2@email2.com'},
+        {'name': 'Джек', 'gender': 'Мужской', 'sport': 'Волейбол', 'email': 'email3@email3.com'}
+    ]
+
+    expected_result = [
+        {'name': 'Сэм', 'gender': 'Мужской', 'sport': 'Баскетбол'},
+        {'name': 'Джек', 'gender': 'Мужской', 'sport': 'Волейбол'}
+    ]
+
+    actual_result = query(
+        friends,
+        select('name', 'gender', 'sport'),
+        field_filter('sport', ['Баскетбол', 'Волейбол']),
+        field_filter('gender', ['Мужской']),
+    )
+
+    assert expected_result == actual_result
